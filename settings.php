dc8eb29478a186c5f0ab7732b01af12f417c25e0
LaughingQuoll <LaughingQuoll@users.noreply.github.com>
2015-11-08 16:11:58 +1100

    Update settings.php
    Added the missing ; on some lines

--- settings.php
+++ settings.php
@@ -3,7 +3,7 @@ $host = 'hostname';
 $user = 'username';
 $pass = 'password';
 $db = 'database';
-$root = 'root address' /* This is your website address. Eg: http://laughingquoll.net */
-$loginplease = 'address' /* This is the address that the pages will go to if the user is not logged in. This will normally be http://hostname/admin/ */
-$administrationemail = 'email' /* The email you want the message system to use as a reply and from email. */
+$root = 'root address'; /* This is your website address. Eg: http://laughingquoll.net */
+$loginplease = 'address'; /* This is the address that the pages will go to if the user is not logged in. This will normally be http://hostname/admin/ */
+$administrationemail = 'email'; /* The email you want the message system to use as a reply and from email. */
 ?>
