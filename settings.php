6435ac02658c6ed8046e15201a92c8fabc6080f8
LaughingQuoll <LaughingQuoll@users.noreply.github.com>
2015-11-08 18:52:44 +1100

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,13 +3,8 @@ $host = 'hostname';
 $user = 'username';
 $pass = 'password';
 $db = 'database';
+$salt = '1diI9sd$ds*72' /* This can be anything. This defines the salt used in the hashing of the passcode. However make sure you change it.*/
 $root = 'root address'; /* This is your website address. Eg: http://laughingquoll.net */
-<<<<<<< HEAD
-$loginplease = 'address' /* This is the address that the pages will go to if the user is not logged in. This will normally be http://hostname/admin/ */
-$administrationemail = 'email' /* The email you want the message system to use as a reply and from email. */
-?>
-=======
 $loginplease = 'address'; /* This is the address that the pages will go to if the user is not logged in. This will normally be http://hostname/admin/ */
 $administrationemail = 'email'; /* The email you want the message system to use as a reply and from email. */
 ?>
->>>>>>> origin/master
